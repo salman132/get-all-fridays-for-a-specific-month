5c9787b4b3700bfce6a693a11b9cf9c54ed96896
Salman Rahman Auvi <36760223+salman132@users.noreply.github.com>
2020-02-06 15:37:20 +0600

    Update getAllFirdaysOfaMonth.php

--- getAllFirdaysOfaMonth.php
+++ getAllFirdaysOfaMonth.php
@@ -2,12 +2,13 @@
     
 $fridays = array();
 
-$date = date('2020-03-01');
-$fridays[0] = date('d',strtotime('first fri of '.$date));
+$date = date('2020-02');
+$day= 'Sunday';
+$fridays[0] = date('d',strtotime("first {$day} of ".$date));
 $fridays[1] = $fridays[0] + 7;
 $fridays[2] =  $fridays[0] + 14;
 $fridays[3] =  $fridays[0] + 21;
-$fridays['last'] = date('d',strtotime('last fri of this month'));
+$fridays['last'] = date('d',strtotime("last {$day} of this month"));
 
 if($fridays[3] == $fridays['last']){
   unset($fridays['last']);
@@ -17,6 +18,9 @@ else {
   unset($fridays['last']);
 }
 
-print_r($fridays);
+foreach($fridays as $off){
+    echo date($date."-".$off);
+    echo "\n";
+}
 
 ?>
